7e9dc30c95694e3287a3e8ce0ed908e79052f998
hosseini.milad1400 <hosseini.milad1400@gmail.com>
2023-07-01 08:41:26 +0330

    pc

--- router/formApi.js
+++ router/formApi.js
@@ -8,7 +8,8 @@ const router = express.Router()
 const auth = require("../middleware/auth");
 const User = require("../models/auth/users");
 const UserDetail = require("../models/auth/customerDetail")
-const UserMontage = require("../models/auth/customerMontage")
+const UserMontage = require("../models/auth/customerMontage");
+const task = require('../models/main/task');
 
 router.post('/user-detail',auth,jsonParser, async (req,res)=>{
   const userId =req.body.userId?req.body.userId:req.headers['userid']
@@ -120,6 +121,10 @@ router.post('/update-user-montage',auth,jsonParser, async (req,res)=>{
   
   try {
         const userMontage = await UserMontage.findOne({userId:ObjectID(data.userId)});
+        
+      //console.log(userTask)
+      await task.updateOne({userId:ObjectID(data.userId),state:"lead"},
+      {$set:{state:"informations"}})
         if(userMontage){
           await UserMontage.updateOne({userId:data.userId},{$set:data});
           res.status(200).json({user:userMontage,message:"UserMortage Updated"})
@@ -128,7 +133,94 @@ router.post('/update-user-montage',auth,jsonParser, async (req,res)=>{
           await UserMontage.create(data);
           res.status(200).json({user:userMontage,message:"UserMortage Created"})
         }
+      
+  } 
+  catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+router.post('/update-user-detail',auth,jsonParser, async (req,res)=>{
+
+  const data={
+    userId:req.body.userId?req.body.userId:req.headers['userid'],
+    birthday: req.body.birthday ,
+    nationality: req.body.nationality ,
+    morada: req.body.morada ,
+    address: req.body.address ,
+
+    profession: req.body.profession ,
+    contract: req.body.contract ,
+    household: req.body.household ,
+    homeContractual: req.body.homeContractual ,
+    academicDegree: req.body.academicDegree ,
+
+    maturity: req.body.maturity,
+    receipts: req.body.receipts,
+    income: req.body.income,
+    otherIncome: req.body.otherIncome,
+
+    mortgageLoans: req.body.mortgageLoans,
+    personalCredit: req.body.personalCredit,
+    carLoan: req.body.carLoan,
+    otherCharges:req.body.otherCharges,
+
+    date:new Date()
+  }
+  
+  try {
+        const userDetails = await UserDetail.findOne({userId:ObjectID(data.userId)});
+        if(userDetails){
+          await UserDetail.updateOne({userId:data.userId},{$set:data});
+          res.status(200).json({user:userDetails,message:"UserDetail Updated"})
+        }
+        else{
+          await UserDetail.create(data);
+          res.status(200).json({user:userDetails,message:"UserDetail Created"})
+        }
       } 
+  catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+router.post('/user-montage',auth,jsonParser, async (req,res)=>{
+  const userId =req.body.userId?req.body.userId:req.headers['userid']
+  try {
+        const userMontage = await UserMontage.aggregate([
+        { $match :({userId:userId})},
+        { $addFields: { "userId": { "$toObjectId": "$userId" }}},
+        {$lookup:{
+            from : "users", 
+            localField: "userId", 
+            foreignField: "_id", 
+            as : "userDetail"
+        }}])
+        if(userMontage){
+          
+          res.status(200).json({user:userMontage,message:"User Exists"})
+          }
+        else{
+          res.status(500).json({error:"User Not Found"})
+        }
+      } 
+  catch(error){
+      res.status(500).json({message: error.message})
+  }
+})
+router.post('/confirm-user-data',auth,jsonParser, async (req,res)=>{
+
+  const data={
+    taskId:req.body.taskId,
+    state: req.body.state,
+    date:new Date()
+  }
+  
+  try {
+        await task.updateOne({_id:req.body.taskId},
+          {$set:{state:req.body.state}})
+          res.status(200).json({message:"User Data Confirmed"})
+        
+      
+  } 
   catch(error){
       res.status(500).json({message: error.message})
   }
